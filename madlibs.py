5a0bd322495906384244e1180b2a9d73830f0c7d
MAC <user@MACs-MacBook-Pro.local>
2022-08-19 19:13:06 +0100

    Madlib
    Created a Madlib

---
+++ madlibs.py
@@ -0,0 +1,15 @@
+# string concatenation (aka How to put strings together)
+
+youtuber= ' Mr shady'
+
+print(f'subscribe to {youtuber}')
+
+adj = input('Adjective: ')
+verb= input('Verb: ')
+verb2= input('Varb: ')
+famous_person= input('Famous persons name')
+
+madlib = f'Computer programming is to {adj}! It makes me so excited all the time because \
+    I love to {verb}. stay hydrated and {verb2} Like you are {famous_person}!'
+
+print(madlib)
